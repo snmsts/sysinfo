aee0973c79d201b16dc3768922ddfc90eadfe875
SANO Masatoshi <snmsts@gmail.com>
2020-04-10 03:56:57 +0900

    implement darwin part

--- darwin.lisp
+++ darwin.lisp
@@ -1,12 +1,16 @@
-(uiop/package:define-package :sysinfo/darwin (:use :cl))
+(uiop/package:define-package :sysinfo/darwin (:nicknames) (:use :cl) (:shadow)
+  (:import-from :sysinfo/posix :system :os :arch
+   :kernel-version)
+  (:export :os :arch :kernel-version :product :build) (:intern))
 (in-package :sysinfo/darwin)
-;;;don't edit above
+;;don't edit above
 
-;; import from posix
-;; arch
-;; os
-;; kernel-version
+(defun product ()
+  "-> 10.15"
+  (string-trim (format nil "~%")
+               (system "sw_vers -productVersion")))
 
-;; tbd functions should be implement wrapping sw_vers.
-;;build(version)
-;;product
+(defun build ()
+  "-> 19A583"
+  (string-trim (format nil "~%")
+               (system "sw_vers -buildVersion")))
